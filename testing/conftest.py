d5d827324c73804b9b46005029cb639bcaa8251e
Andreas Motl <andreas.motl@panodata.org>
2022-08-01 00:53:39 +0200

    CLI: Use extended settings also for CLI entrypoints `info` and `explore`

--- testing/conftest.py
+++ testing/conftest.py
@@ -1,6 +1,8 @@
 # -*- coding: utf-8 -*-
 # (c) 2022 Andreas Motl <andreas.motl@panodata.org>
 # License: GNU Affero General Public License, Version 3
+import os
+
 import pytest
 from pytest_mock import MockerFixture
 
@@ -12,3 +14,11 @@ def use_stable_cross_platform_bluetooth_adapter(mocker: MockerFixture):
     Otherwise, the outcome will deviate on Linux vs. macOS.
     """
     mocker.patch("calypso_anemometer.core.get_adapter_name", return_value="hci0")
+
+
+@pytest.fixture(autouse=True)
+def clean_environment_variables(mocker: MockerFixture):
+    """
+    Make sure the tests will run with a deterministic set of application-specific environment variables.
+    """
+    mocker.patch.dict(os.environ, {"CALYPSO_QUIET": "false"})
